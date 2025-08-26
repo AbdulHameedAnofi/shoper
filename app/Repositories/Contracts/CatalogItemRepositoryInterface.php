dc83e7919b10ef36063fdef02509db6299a9eb2e
Abdulhameed <anofiolawale@gmail.com>
2025-08-26 13:08:55 +0100

    added .htacess for startup file lookup

--- app/Repositories/Contracts/CatalogItemRepositoryInterface.php
+++ app/Repositories/Contracts/CatalogItemRepositoryInterface.php
@@ -2,7 +2,7 @@
 
 namespace App\Repositories\Contracts;
 
-interface CategoryItemRepositoryInterface
+interface CatalogItemRepositoryInterface
 {
     public function addItem(string $catalog_uuid, string $item_uuid);
     public function removeItem(string $catalog_uuid, string $item_uuid);
